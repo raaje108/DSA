8ba6368a8417af820b136771731b6a02b637672a
raaje108 <105787105+raaje108@users.noreply.github.com>
2026-07-14 22:16:23 +0530

    Time: 2 ms (51.29%), Space: 8.5 MB (99.28%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.c
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.c
@@ -9,18 +9,14 @@ char* mergeAlternately(char* word1, char* word2) {
 
     int i = 0, j = 0, k = 0;
 
-    while (i < n1 && j < n2) {
-        ans[k++] = word1[i++];
-        ans[k++] = word2[j++];
+    while (i < n1 || j < n2) {
+        if (i < n1)
+            ans[k++] = word1[i++];
+
+        if (j < n2)
+            ans[k++] = word2[j++];
     }
 
-    while (i < n1)
-        ans[k++] = word1[i++];
-
-    while (j < n2)
-        ans[k++] = word2[j++];
-
     ans[k] = '\0';
-
     return ans;
 }
