ca00bddf31d007890109aa06add26e7605b72646
raaje108 <105787105+raaje108@users.noreply.github.com>
2026-07-25 22:50:19 +0530

    Time: 4 ms (18.41%), Space: 30.9 MB (61.69%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.c
@@ -0,0 +1,21 @@
+int* productExceptSelf(int* nums, int numsSize, int* returnSize) {
+    *returnSize = numsSize;
+
+    int *ans = (int *)malloc(numsSize * sizeof(int));
+
+    ans[0] = 1;
+
+    // Prefix products
+    for (int i = 1; i < numsSize; i++) {
+        ans[i] = ans[i - 1] * nums[i - 1];
+    }
+
+    // Suffix products
+    int suffix = 1;
+    for (int i = numsSize - 1; i >= 0; i--) {
+        ans[i] *= suffix;
+        suffix *= nums[i];
+    }
+
+    return ans;
+}
