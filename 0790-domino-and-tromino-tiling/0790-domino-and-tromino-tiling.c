835027d4fb34b4a77f0c8e3004ed670c5f63fd12
raaje108 <105787105+raaje108@users.noreply.github.com>
2026-08-12 22:58:50 +0530

    Time: 0 ms (100%), Space: 8.6 MB (34.21%) - LeetHub

---
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.c
@@ -0,0 +1,21 @@
+int numTilings(int n) {
+    const int MOD = 1000000007;
+
+    if (n == 1)
+        return 1;
+
+    if (n == 2)
+        return 2;
+
+    long long dp[n + 1];
+
+    dp[0] = 1;
+    dp[1] = 1;
+    dp[2] = 2;
+
+    for (int i = 3; i <= n; i++) {
+        dp[i] = (2 * dp[i - 1] + dp[i - 3]) % MOD;
+    }
+
+    return dp[n];
+}
